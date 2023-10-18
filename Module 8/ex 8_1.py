0ab3a5707b50a2d34cc54df1c2a463ffbe6329b0
VitaliiVIP <vitalii.virronen@yahoo.com>
2023-10-19 02:02:59 +0300

    hello

--- Module 8/ex 8_1.py
+++ Module 8/ex 8_1.py
@@ -1,1 +1,23 @@
+import mysql.connector
 
+connection = mysql.connector.connect(
+         host='127.0.0.1',
+         port= 3306,
+         database='flight_game',
+         user='root',
+         password='vit2357rew',
+         autocommit=True
+         )
+def f(ident):
+    sql = "SELECT name, iso_region from airport"
+    sql += " WHERE ident='" + ident+"'"
+    print(sql)
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    if cursor.rowcount >0 :
+        for row in result:
+            print(f"Name - {row[0]}\nLocation - {row[1]} ")
+    return
+ident = input("Enter icao: ")
+f(ident)
